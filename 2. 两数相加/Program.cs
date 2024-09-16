c3357cbe4385773178147f2b168905d6a6b4a428
王琪 <208277594@qq.com>
2024-09-16 15:50:42 +0800

    ce

---
+++ 2. 两数相加/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.两数相加
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+             给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+            请你将两个数相加，并以相同形式返回一个表示和的链表。
+            你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+            输入：l1 = [2,4,3], l2 = [5,6,4]
+            输出：[7,0,8]
+            解释：342 + 465 = 807.
+             */
+            var l1 = new ListNode(9, new ListNode(9, new ListNode(9)));
+            var l2 = new ListNode(9, new ListNode(9, new ListNode(9,new ListNode(9))));
+            Solution solution = new Solution();
+            var res = solution.AddTwoNumbers(l1, l2);
+
+            while (res != null)
+            {
+                Console.WriteLine(res.val);
+                res = res.next;
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode(0);
+            ListNode current = head;
+            int sum = 0;
+            int jinwei = 0;
+            while (l1 != null || l2 != null)
+            {
+                int a = l1 == null ? 0 : l1.val;
+                int b = l2 == null ? 0 : l2.val;
+                sum =  jinwei + a + b;
+                jinwei = sum >= 10 ? 1 : 0;
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+            if (jinwei!=0)
+            {
+                current.next = new ListNode(jinwei);
+            }
+            return head.next;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ 20. 有效的括号/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.有效的括号
+{
+    internal class Program
+    {
+        /*
+         给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+        有效字符串需满足：
+
+        左括号必须用相同类型的右括号闭合。
+        左括号必须以正确的顺序闭合。
+        每个右括号都有一个对应的相同类型的左括号。
+
+
+        示例 1：
+
+        输入：s = "()"
+
+        输出：true
+
+        示例 2：
+
+        输入：s = "()[]{}"
+
+        输出：true
+
+        示例 3：
+
+        输入：s = "(]"
+
+        输出：false
+
+        示例 4：
+
+        输入：s = "([])"
+
+        输出：true
+
+
+
+        提示：
+
+        1 <= s.length <= 104
+        s 仅由括号 '()[]{}' 组成
+         */
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+        }
+
+
+    }
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+            Stack<char> stack = new Stack<char>();
+            foreach (var item in s)
+            {
+                if (item == '{' || item == '(' || item == '[')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')' && stack.Count > 0 && stack.Peek() == '(')
+                {
+                    stack.Pop();
+
+                }
+                else if (item == ']' && stack.Count > 0 && stack.Peek() == '[')
+                {
+                    stack.Pop();
+
+
+                }
+                else if (item == '}' && stack.Count > 0 && stack.Peek() == '{')
+                {
+                    stack.Pop();
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if (stack.Count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
